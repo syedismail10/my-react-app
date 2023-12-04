39d86a0226b123c3256356503682c316457d91cc
syedismail10 <syedismailulhaq@gmail.com>
2023-12-04 22:19:35 +0500

    final commits before editing

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,36 +1,68 @@
-import {createStore , combineReducers, applyMiddleware} from "redux"
-import thunk from "redux-thunk"
-import {composeWithDevTools} from "redux-devtools-extension"
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import {
-    productDetailsReducer,
-    productListReducer,
-
-}from "./reducers/ProductReducers";
+  productCreateReviewReducer,
+  productDetailsReducer,
+  productListReducer,
+} from "./Reducers/ProductReducers";
 import { cartReducer } from "./Reducers/CartReducers";
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from "./Reducers/userReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderListMyReducer,
+  orderPayReducer,
+} from "./Reducers/OrderReducres";
 
 const reducer = combineReducers({
-    productList : productListReducer,
-    productDetails: productDetailsReducer,
-    cart:cartReducer,
-
-const cartItemsFromLocalStorage = localStorage.getItem("cartItem")
-?JSON.parse(localStotage.getItem("cartItem"))
-:[]
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productReviewCreate: productCreateReviewReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 });
 
-const initialState={
-cart :{
-    cartItems: cartItemsFromLocalStorage
- },
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+// login
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+// shippingAddress
+const shippingAddressFromLocalStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromLocalStorage,
+    shippingAddress: shippingAddressFromLocalStorage,
+  },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
-const middleware= [thunk]
 
-const store = createtStore(
-    reducer,
-    intialState,
-    composeWithDevtools(applyMiddleware(...middleware))
+const middleware = [thunk];
 
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -1,37 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { savePaymentMethod } from "../Redux/Actions/cartActions";
+import Header from "./../components/Header";
 
-const PaymentScreen =() => {
-  window.scrollTo(0,0);
-  const submitHandler = (e) =>{
-    e.preventDefault()
+const PaymentScreen = ({ history }) => {
+  window.scrollTo(0, 0);
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    history.push("/shipping");
   }
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container d-flex justify-content-center align-items-center login-center">
-        <form className="Login2 col-md-8 col-lg-4 col-11" onSubmit={submitHandler}>
-          <h6>select payment method</h6>
+        <form
+          className="Login2 col-md-8 col-lg-4 col-11"
+          onSubmit={submitHandler}
+        >
+          <h6>SELECT PAYMENT METHOD</h6>
           <div className="payment-container">
             <div className="radio-container">
-              <input type="radio" value='Easypaisa' className="form-check-input" />
-              <label className="form-check-label">Easypaisa</label>
-            </div>
-            <div className="radio-container">
-              <input type="radio" value='COD' className="form-check-input" />
-              <label className="form-check-label"> COD</label>
+              <input
+                className="form-check-input"
+                type="radio"
+                value={paymentMethod}
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              />
+              <label className="form-check-label">PayPal or Credit Card</label>
             </div>
           </div>
 
-          <button type="submit">
-            <Link to ='/placeorder' className='text-white'>
-              Continue
-            </Link>
-          </button>
+          <button type="submit">Continue</button>
         </form>
       </div>
     </>
-  )
-}
-export default PaymentScreen
+  );
+};
+
+export default PaymentScreen;
