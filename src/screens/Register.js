23b2917d298d25aa79616929020b3efe7b3bdb03
syedismail10 <syedismailulhaq@gmail.com>
2023-12-04 16:26:47 +0500

    commit

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,14 +3,23 @@ import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 const Register = () => {
   window.scrollTo(0,0)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+  }
   return (
     <>
     <Header/>
     <div className="container d-flex flex-column justify-content-center align-items-center">
-      <form className="Login col-md-8 col-lg-4 col-11">
+      <form className="Login col-md-8 col-lg-4 col-11
+      " onSubmit={submitHandler}>
+      <input type="text" placeholder='Name'/>
         <input type="text" placeholder='username'/>
         <input type="email" placeholder='Email'/>
+        <input type="text" placeholder='Phone number'/>
         <input type="password" placeholder='Password'/>
+        <input type="text" placeholder='Address'/>
+        <input type="text" placeholder='City'/>
 
         <button type='submit'>register</button>
         <p>
